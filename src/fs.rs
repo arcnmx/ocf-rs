198cf5f2fc22515aa46fe67eb93a766f87dcfbc4
Luca Bruno <lucab@debian.org>
2017-02-12 01:11:39 +0000

    cargo: update to serde 0.9
    This updates to latest serde version (0.9). This retains
    compatibility with older rustc (<1.15) via codegen, but it can be
    dropped at a later time.

--- src/fs.rs
+++ src/fs.rs
@@ -7,16 +7,11 @@ use runtime::RuntimeSpec;
 
 pub fn load<P: AsRef<Path>>(p: P) -> io::Result<(Spec, RuntimeSpec)> {
     fn err(e: serde_json::Error) -> io::Error {
-        match e {
-            serde_json::Error::Io(e) => e,
-            e => io::Error::new(io::ErrorKind::InvalidData, e),
-        }
+        io::Error::new(io::ErrorKind::InvalidData, e)
     }
     let p = p.as_ref();
     let config = try!(File::open(p.join("config.json")));
     let runtime = try!(File::open(p.join("runtime.json")));
-    Ok((
-        try!(Spec::from_read(config).map_err(err)),
-        try!(RuntimeSpec::from_read(runtime).map_err(err)),
-    ))
+    Ok((try!(Spec::from_read(config).map_err(err)),
+        try!(RuntimeSpec::from_read(runtime).map_err(err))))
 }
